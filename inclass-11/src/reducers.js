693233d272c5f6806a85f187309a58b0e6897a0f
mamama797979 <xm10@rice.edu>
2016-09-28 08:42:45 +0800

    Add files via upload

---
+++ inclass-11/src/reducers.js
@@ -0,0 +1,41 @@
+
+const Reducer = (state =  {
+	nextId: 2,
+	todoItems: [
+	    {id: 0, text: "This is an item", done: false},
+	    {id: 1, text: "Another item", done: false}
+	]
+}, action) => {
+	switch(action.type) {
+		case 'ADD_TODO':
+			return {
+				nextId: state.nextId+1,
+				todoItems: [
+					...state.todoItems,
+					{id: state.nextId, text: action.text, done:false}
+				]
+			}
+		case 'REMOVE_TODO':
+			return {
+				nextId: state.nextId,
+				todoItems: state.todoItems.filter((todo)=>{
+					return todo.id!=action.id
+				})
+			}
+		case 'TOGGLE_TODO':
+			return {
+				nextId: state.nextId,
+				todoItems: state.todoItems.map((todo) => {
+					if(todo.id==action.id){
+						return {...todo, done: !todo.done}
+					} else{
+						return todo
+					}					
+				})
+			}
+		default: 
+			return state
+	}
+}
+
+export default Reducer
