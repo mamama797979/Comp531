5011fc2a70ab50a8c1a308a5f7c1f09e65472eca
mamama797979 <xm10@rice.edu>
2016-11-09 09:49:30 +0800

    Create landing.js

---
+++ hw6/frontend/src/components/auth/landing.js
@@ -0,0 +1,45 @@
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+import Login from './login'
+import Register from './register'
+
+//The landing page JSX
+let ErrorMessage = ({error, success}) => (
+    <div className="row">
+        { error.length == 0 ? '' :
+            <div className="alert_error">
+                <div id="errorMessage">{ error }</div>
+            </div>
+        }
+        { success.length == 0 ? '' :
+            <div className="alert_success">
+                <div id="successMessage">{ success }</div>
+            </div>
+        }
+    </div>
+)
+ErrorMessage.propTypes = {
+    error: PropTypes.string.isRequired,
+    success: PropTypes.string.isRequired
+}
+ErrorMessage = connect((state) => {
+    return { error: state.common.error, success: state.common.success }
+})(ErrorMessage)
+
+const Landing = () => (
+    <div>
+        <div>&nbsp;</div>
+        <div>&nbsp;</div>
+        <div className="page_title">
+            <h1>RiceNET</h1>
+        </div>
+        <div><ErrorMessage/></div>
+        <h3>Log in:</h3>
+        <Login/>
+        <h3>Registration:</h3>
+        <Register/>
+    </div>
+)
+
+export default Landing
