5f077d8f435f23f76718ab51e077ef62835f860b
mamama797979 <xm10@rice.edu>
2016-11-09 09:59:09 +0800

    Create profileActions.js

---
+++ hw6/frontend/src/components/profile/profileActions.js
@@ -0,0 +1,109 @@
+import Action, { updateError, resource } from '../../actions'
+
+//Operations to syn server and page for the profile data.
+export function validateProfile({username, email, phone, zipcode, password, pwconf}) {
+    if (username) {
+        if (!username.match('^[a-zA-Z][a-zA-Z0-9]+')) {
+            return 'Invalid username.  Must start with a letter and can only contains letters and numbers.'
+        }
+    }
+
+    if (email) {
+        if (!email.match('^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z][a-zA-Z]+$')) {
+            return 'Invalid email.  Email must be in format of a@b.c'
+        }
+    }
+
+    if (phone) {
+        if (!phone.match('^\[0-9]{3}[-]?\[0-9]{3}[-]?\[0-9]{4}$')) {
+            return 'Invalid phone.  Phonr number must be in format of 123-123-1234'
+        }
+    }
+
+    if (zipcode) {
+        if (!zipcode.match('^[0-9]{5}$')) {
+            return 'Invalid zipcode.  Must be 5 digits in length,'
+        }
+    }
+
+    if (password || pwconf) {
+        if (password !== pwconf) {
+            return 'Password do not match'
+        }
+    }
+
+    return ''
+}
+
+export function updateHeadline(headline) {
+    return (dispatch) => {
+        dispatch(updateField('headline', headline))
+    }
+}
+
+export function updateProfile({email, phone, zipcode, password, pwconf}) {
+    return (dispatch) => {
+        const err = validateProfile({email, phone, zipcode, password, pwconf})
+        if (err.length > 0) {
+            return dispatch(updateError(err))
+        }
+        dispatch(updateField('email', email))
+        dispatch(updateField('zipcode', zipcode))
+        dispatch(updateField('password', password))
+    }
+}
+
+export function fetchProfile() {
+    return (dispatch) => {
+        dispatch(fetchField('avatars'))
+        dispatch(fetchField('zipcode'))
+        dispatch(fetchField('email'))
+    }
+}
+
+function updateField(field, value) {
+    return (dispatch) => {
+        if (value) {
+            const payload = {}
+            payload[field] = value
+            resource('PUT', field, payload).then((response) => {
+                const action = { type: Action.UPDATE_PROFILE }                
+                action[field] = response[field]
+                if (field == 'password')
+                    dispatch(updateError('will not change password'))
+                else
+                    dispatch(action)
+            })
+        }
+    }
+}
+
+function fetchField(field) {
+    return (dispatch) => {
+        resource('GET', field).then((response) => {
+            const action = { type: Action.UPDATE_PROFILE }
+            switch(field) {
+                case 'avatars':
+                    action.avatar = response.avatars[0].avatar; break;
+                case 'email':
+                    action.email = response.email; break;
+                case 'zipcode':
+                    action.zipcode = response.zipcode; break;
+            }
+            dispatch(action)
+        })
+    }
+}
+
+export function uploadImage(file) {
+    return (dispatch) => {
+        if (file) {
+            const fd = new FormData()
+            fd.append('image', file)
+            resource('PUT', 'avatar', fd, false)
+            .then((response) => {
+                dispatch({ type: Action.UPDATE_PROFILE, avatar: response.avatar })
+            })
+        }
+    }
+}
