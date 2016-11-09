20d1c92603b86d04e2902e6dd34c64355e5098ef
mamama797979 <xm10@rice.edu>
2016-11-09 10:00:14 +0800

    Create profileForm.js

---
+++ hw6/frontend/src/components/profile/profileForm.js
@@ -0,0 +1,80 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+import { updateProfile } from './profileActions'
+
+class ProfileForm extends Component {
+
+    componentDidUpdate() {
+        if (this.props.error.length == 0) {
+            this.email.value = null
+            this.phone.value = null
+            this.zipcode.value = null
+            this.password.value = null
+            this.pwconf.value = null
+        }
+    }
+
+    render() { return (
+        <form onSubmit={(e) => {
+            if (e) e.preventDefault()
+            const payload = {
+                email:this.email.value == this.oldEmail ? '' : this.email.value,
+                zipcode:this.zipcode.value == this.oldZipcode ? '' : this.zipcode.value,
+                password:this.password.value,
+                pwconf:this.pwconf.value
+            }
+            this.props.dispatch(updateProfile(payload))
+        }}>
+            
+            <table className="index_table1">
+            <tbody>
+                <tr>
+                    <td>Email Address</td>
+                    <td><input id="email" type="text" placeholder={this.props.oldEmail}
+                        ref={(node) => this.email = node }/></td>
+                </tr>
+                <tr>
+                    <td>Zipcode</td>
+                    <td><input id="zipcode" type="text" placeholder={this.props.oldZipcode}
+                        ref={(node) => this.zipcode = node }/></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input id="password" type="password" placeholder="Password"
+                        ref={(node) => this.password = node }/></td>
+                </tr>
+                <tr>
+                    <td>Password Confirmation</td>
+                    <td><input id="pwconf" type="password" placeholder="Password Confirmation"
+                        ref={(node) => this.pwconf = node }/></td>
+                </tr>
+            </tbody>
+            </table>
+            <br />
+            <div className="btnprofile">
+                <button className="button" type="submit">Update</button> 
+            </div>
+
+        </form>
+    )}
+}
+
+ProfileForm.propTypes = {
+    error: PropTypes.string.isRequired,
+    oldZipcode: PropTypes.number.isRequired,
+    oldEmail: PropTypes.string.isRequired,
+    dispatch: PropTypes.func.isRequired
+}
+
+export default connect(
+    (state) => {
+        return {
+            error: state.common.error,
+            oldZipcode: state.profile.zipcode,
+            oldEmail: state.profile.email,
+        }
+    }
+)(ProfileForm)
+
+export { ProfileForm as PureProfileForm }
