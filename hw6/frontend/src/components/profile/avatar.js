3eef615ed3a447f63ef433c6069f4535f79bf1bb
mamama797979 <xm10@rice.edu>
2016-11-09 10:34:20 +0800

    Update avatar.js

--- hw6/frontend/src/components/profile/avatar.js
+++ hw6/frontend/src/components/profile/avatar.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 
 import { uploadImage } from './profileActions'
 
-//Avatar JSX
 class Avatar extends Component {
 
     componentDidUpdate(oldprops) {
@@ -27,28 +26,30 @@ class Avatar extends Component {
         reader.readAsDataURL(this.file)
     }
 
-    render() { return (
-        <div className="index_table2">
-            <img width="100%" height="50%" src={this.props.img}/>
-            <em>Upload new avatar</em>
-            <p><input type="file" accept="image/*" onChange={(e) => this.handleImageChange(e)}/></p>
-            <div>
-            { !this.file ? '' :
+    render() {
+     return (
+        <div className="left_box">
+            <div className="user_card">
+                <img id="profileImg" width="100%" src={this.props.img}/>
+                <em>Upload new profile avatar</em>
+                <p><input type="file" accept="image/*" onChange={(e) => this.handleImageChange(e)}/></p>
                 <div>
+                { !this.file ? '' :
                     <div>
-                        <img width="100%" src={this.preview}/>
+                        <div>
+                            <img width="100%" src={this.preview}/>
+                        </div>
+                        <div>
+                            { this.file.webkitRelativePath || this.file.name } ({ parseInt(this.file.size / 1024 * 100)/100.0 } kB)
+                        </div>
+                        <input className="card_button" type="button" 
+                        value="Update Profile Picture" 
+                        onClick={() => { this.props.dispatch(uploadImage(this.file)) }}/>
                     </div>
-                    <div>
-                        { this.file.webkitRelativePath || this.file.name } ({ parseInt(this.file.size / 1024 * 100)/100.0 } kB)
-                    </div>
-                    <input className="card_button" type="button" value="Update Profile Picture" onClick={() => { this.props.dispatch(uploadImage(this.file)) }}/>
-                </div>
-            }
+                }
+                </div>      
             </div>
-            <p>Xintong Ma</p>
-            <p>05/08/1993</p>         
         </div>
-
     )}
 }
 
