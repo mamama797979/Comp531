017358159de7157e1eae87fa9bc56f7074a9e2f1
mamama797979 <xm10@rice.edu>
2016-11-09 10:33:07 +0800

    Update headline.js

--- hw6/frontend/src/components/main/headline.js
+++ hw6/frontend/src/components/main/headline.js
@@ -2,7 +2,6 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { updateHeadline } from '../profile/profileActions'
 
-//User's headline react component
 class Headline extends Component {
 
     render() { return (
@@ -12,14 +11,14 @@ class Headline extends Component {
             <p id="headline">{ this.props.headline }</p>
             <div>
                 <div>
-                    <p><input className="headfield" id="headline" type="text"
+                    <p><input className="headfield" id="newheadline" type="text"
                         placeholder="update your headline"
                         ref={ (node) => { this.newHeadline = node }}
                         onChange={() => this.forceUpdate()} /></p>
                 </div>
                 { !(this.newHeadline && this.newHeadline.value.length > 0) ? '' :
                     <div>
-                        <p><input className="udtbtn" type="button" value="Update your Headline"
+                        <p><input className="udtbtn" id="headlineBtn" type="button" value="Update your Headline"
                             onClick={() => {
                                 this.props.dispatch(updateHeadline(this.newHeadline.value))
                                 this.newHeadline.value = ''
