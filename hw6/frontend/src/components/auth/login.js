75108b63c71e87943feda94adae3368c44d86b8a
mamama797979 <xm10@rice.edu>
2016-11-09 10:31:51 +0800

    Update login.js

--- hw6/frontend/src/components/auth/login.js
+++ hw6/frontend/src/components/auth/login.js
@@ -3,28 +3,25 @@ import { connect } from 'react-redux'
 
 import { localLogin } from './authActions'
 
-//The login section react component
 const Login = ({dispatch}) => {
     let username, password
     return (
         <div className="login_title">
             <table className="index_table">
-            <tbody>
                 <tr>
                     <td>Account Name</td>
-                    <td><input id="loginUsername" type="text" placeholder="Account Name"
+                    <td><input id="loginUsername" type="text" placeholder="username"
                         ref={(node) => { username = node }} /></td>
                 </tr>
                 <tr>
                     <td>Password</td>
-                    <td><input id="loginPassword" type="password" placeholder="Password"
+                    <td><input id="loginPassword" type="password" placeholder="password"
                         ref={(node) => { password = node }} /></td>
-                </tr> 
-            </tbody>  
+                </tr>   
             </table>
             <div>&nbsp;</div>
             <div className="btnsignin">
-                <input className="button" type="button" value="Log In"
+                <input className="button" id="loginButton" type="button" value="Log In"
                 onClick={() => { dispatch(localLogin(username.value, password.value)) }}/>
             </div>
         </div>            
