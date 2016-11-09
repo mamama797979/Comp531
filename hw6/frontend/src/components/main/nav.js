3a0fbcd452e6ec42add6de04e0b0342df01b089e
mamama797979 <xm10@rice.edu>
2016-11-09 10:33:32 +0800

    Update nav.js

--- hw6/frontend/src/components/main/nav.js
+++ hw6/frontend/src/components/main/nav.js
@@ -3,22 +3,23 @@ import { connect } from 'react-redux'
 import { navToMain, navToProfile } from '../../actions'
 import { logout } from '../auth/authActions'
 
-const Nav = ({username, onProfile, onMain, dispatch}) => (
+const Nav = ({username, onProfile, dispatch}) => (
     <nav>
       <div>
-        { <div>      
-            {onProfile?
-              <div>
-                <li className="button"><a href="#" onClick={() => { dispatch(navToMain()) }}>Main page</a></li>
-                <li className="button"><a href="#" onClick={() => { dispatch(logout()) }}>Log out</a></li>
-              </div>:''               
+        {/*
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          </button>
+          <a className="navbar-brand" href="#">DH Inc.</a>
+        </div>
+        */}
+        { username.length == 0 ? '' :
+          <div>      
+            { onProfile ?
+              <li className="button" id="navHome"><a href="#" onClick={() => { dispatch(navToMain()) }}>Home</a></li> :
+              <li className="button" id="navProfile"><a href="#" onClick={() => { dispatch(navToProfile()) }}>Edit Your Profile</a></li>
             }
-            {onMain?
-              <div>
-                <li className="button"><a href="#" onClick={() => { dispatch(navToProfile()) }}>Profile Page</a></li>
-                <li className="button"><a href="#" onClick={() => { dispatch(logout()) }}>Log out</a></li>
-              </div>:'' 
-            }
+            <li className="button" id="navLogout"><a href="#" onClick={() => { dispatch(logout()) }}>Log out {username} </a></li>
           </div>
         }
       </div>
@@ -29,6 +30,5 @@ export default connect(
   (state) => {
     return {
       username: state.profile.username || '',
-      onProfile: state.common.location == 'profile',
-      onMain: state.common.location == 'main'}
+      onProfile: state.common.location == 'profile' }
   })(Nav)
