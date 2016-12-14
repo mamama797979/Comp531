1664556c82b642fb51722b6c514044274b1a518e
mamama797979 <xm10@rice.edu>
2016-12-13 21:58:56 -0600

    Add files via upload

---
+++ hw8/frontend/src/components/profile/unlinkBtn.js
@@ -0,0 +1,32 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+import { unlinkAccount } from './profileActions'
+
+class UnlinkBtn extends Component {
+
+	render() { return (
+		<div>
+			{this.props.loginUser.split('@')[1] ? '' :
+				<div className="form-group row">
+		            <div className="col-sm-6">
+		                <button className="btn btn-warning" type="button"
+		                	onClick = {() => {this.props.dispatch(unlinkAccount('facebook'))}}>UnLink with Facebook</button>
+		            </div>
+		        </div>
+		    }
+	    </div>
+	)}
+}
+
+UnlinkBtn.propTypes = {
+		loginUser: PropTypes.string.isRequired
+}
+
+export default connect(
+	(state) => {
+		return {
+			loginUser: state.profile.username || ''
+		}
+	}
+)(UnlinkBtn)
